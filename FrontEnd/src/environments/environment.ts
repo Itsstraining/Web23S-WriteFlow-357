d549187b5a738286c7c6745dfc76ffc453a13965
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-03-03 18:19:34 +0700

    add environment

---
+++ FrontEnd/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCcIwUtNcrK4_N0eLE5p7qFU9FuJaJFOS8",
+        authDomain: "writeflow-7cb01.firebaseapp.com",
+        projectId: "writeflow-7cb01",
+        storageBucket: "writeflow-7cb01.appspot.com",
+        messagingSenderId: "92118223652",
+        appId: "1:92118223652:web:a7e8f8a1052010cf0a81b1"
+    }
+};
