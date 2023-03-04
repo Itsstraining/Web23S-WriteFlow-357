04533dc3b89a3c0be91a9d763b40b485a4c856ea
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-03-04 10:52:46 +0700

    Base style quill

--- FrontEnd/src/app/pages/main/components/document/document.component.ts
+++ FrontEnd/src/app/pages/main/components/document/document.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { QuillModule } from 'ngx-quill';
 
 @Component({
   selector: 'app-document',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./document.component.scss']
 })
 export class DocumentComponent {
+  @ViewChild('quillEditor') editor: any;
 
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    let editor = this.editor.elementRef.nativeElement;
+    let container = editor.getElementsByClassName('ql-container')[0];
+  }
+
+  quillModule: QuillModule = {
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+        ['code-block'],
+        [{ header: 1 }, { header: 2 }], // custom button values
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+        //[{ 'direction': 'rtl' }],                         // text direction
+        //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        //[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        //[{ 'font': [] }],
+        //[{ 'align': [] }],
+        ['clean'], // remove formatting button
+        ['link'],
+        //['link', 'image', 'video']
+      ],
+    },
+  }
 }
