ad73d645cdd3c7cb61cff37d37b1272b1ca2ee39
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-03-08 18:36:25 +0700

    Add profile

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
 
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
 
   { path: 'main', loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule) },
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
 
 
   { path: '**', loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundModule) },
--- FrontEnd/src/app/pages/home/home.component.ts
+++ FrontEnd/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,5 +9,21 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router, private authService: AuthService) { }
 
+  user: User | null = null;
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser;
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+      if (user) {
+        this.router.navigate(['/main']);
+      }
+    });
+
+    if (this.user) {
+      this.router.navigate(['/main']);
+    }
+  }
 }
