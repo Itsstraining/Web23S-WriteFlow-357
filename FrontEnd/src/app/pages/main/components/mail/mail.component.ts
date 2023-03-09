e321931f9260c4dd5e7236b09fd393b6143913dc
BP <chuminhkhoi3110@gmail.com>
2023-03-09 15:33:49 +0700

    fix email page

--- FrontEnd/src/app/pages/main/components/mail/mail.component.ts
+++ FrontEnd/src/app/pages/main/components/mail/mail.component.ts
@@ -9,7 +9,18 @@ export class MailComponent {
 
   constructor(){}
 
+  panelOpenState = false;
   reloadEmail(){
     location.reload();
+  }
+
+  AcceptRequest()
+  {
+    window.alert('You have accepted the invitation');
+  }
+
+  RejectRequest()
+  {
+    window.alert('You have been denied the invitation');
   }
 }
