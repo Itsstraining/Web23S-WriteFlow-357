e2639416cff7aa0e192aa6a31286f8bb39a2ae81
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-03-10 16:07:54 +0700

    .

--- FrontEnd/src/app/modules/angular-material/angular-material.module.ts
+++ FrontEnd/src/app/modules/angular-material/angular-material.module.ts
@@ -7,17 +7,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -36,7 +36,7 @@ import {MatMenuModule} from '@angular/material/menu';
     MatExpansionModule,
     MatBadgeModule,
     MatStepperModule,
-    MatSelectModule
+    MatSelectModule,
     MatSnackBarModule,
     MatSidenavModule,
     MatListModule,
@@ -55,7 +55,7 @@ import {MatMenuModule} from '@angular/material/menu';
     MatExpansionModule,
     MatBadgeModule,
     MatStepperModule,
-    MatSelectModule
+    MatSelectModule,
     MatSnackBarModule,
     MatSidenavModule,
     MatListModule,
