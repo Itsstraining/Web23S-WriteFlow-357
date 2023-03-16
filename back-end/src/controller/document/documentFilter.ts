a3df9e5fd64d5a79f8c52dbf44639d39be51f309
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-03-16 12:31:49 +0700

    add try catch file

--- back-end/src/controller/document/documentFilter.ts
+++ back-end/src/controller/document/documentFilter.ts
@@ -9,36 +9,47 @@ const { v4: uuidv4 } = require('uuid');
 
 import * as fs from "fs";
 import * as path from "path";
+import { HttpException } from "@nestjs/common";
 
 export const saveDocumentToStorage = {
     storage: diskStorage({
         destination: (req, file, cb) => {
-            const pathToImageFolder = path.join('src', 'documentsStorage', req.headers.ownerid as string);
+            try {
+                const pathToImageFolder = path.join('src', 'documentsStorage', req.headers.ownerid as string);
 
-            if (!fs.existsSync(pathToImageFolder)) {
-                fs.mkdirSync(pathToImageFolder)
+                if (!fs.existsSync(pathToImageFolder)) {
+                    fs.mkdirSync(pathToImageFolder)
+                }
+
+                cb(null, pathToImageFolder);
             }
-
-            cb(null, pathToImageFolder);
+            catch (error) {
+                throw new HttpException('Bad request', 400, { cause: new Error("Forbidden") });
+            }
         },
         filename: (req, file, cb) => {
-            let ownderid = req.headers.ownerid as string;
-            let fullNameFile = req.headers.filename as string;
-            let filePath = '';
+            try {
+                let ownderid = req.headers.ownerid as string;
+                let fullNameFile = req.headers.filename as string;
+                let filePath = '';
 
-            if (fullNameFile) {
-                filePath = path.join('src', 'documentsStorage', ownderid, fullNameFile);
+                if (fullNameFile) {
+                    filePath = path.join('src', 'documentsStorage', ownderid, fullNameFile);
+                }
+
+                if (fs.existsSync(filePath) && fullNameFile) {
+                    cb(null, fullNameFile);
+                    return;
+                }
+
+                const fileExtension = path.extname(file.originalname);
+                const fileName = uuidv4() + (fileExtension ? fileExtension : '.json');
+
+                cb(null, fileName);
             }
-
-            if (fs.existsSync(filePath) && fullNameFile) {
-                cb(null, fullNameFile);
-                return;
+            catch (error) {
+                throw new HttpException('Bad request', 400, { cause: new Error("Forbidden") });
             }
-
-            const fileExtension = path.extname(file.originalname);
-            const fileName = uuidv4() + (fileExtension ? fileExtension : '.json');
-
-            cb(null, fileName);
         }
     }),
     fileFilter: (req, file, cb) => {
